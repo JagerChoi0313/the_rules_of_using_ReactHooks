d6f4ceede723fa524b2b53bcca4d08861deb1dc6
John Do <17322905680@qq.com>
2026-01-06 07:40:41 +0800

    the_rules_0f_using_ReactHooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+//ReactHooks使用规则
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { use } from "react"
+
+//使用规则
+//1.只能在组件中或者其他自定义Hook函数中调用
+//2.只能在组件的顶层调用，不能嵌套在if，for，其他函数中
+
+//错误用法1：组件外使用
+//错误用法2：if for 组件内部函数
+
+import {useState} from 'react'
+
+//useState 会报错
+
+function App(){
+  // if(Math.random()>0.5)
+  // {
+  //   useState()//会报错
+  // }
+  return(
+    <div>
+      This is APP
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
